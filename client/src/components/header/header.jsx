5ecf9b9d5d844256923507495b15c0b5bad1cff8
Sandy Nguyen <69399429+sandyngu@users.noreply.github.com>
2021-06-15 13:30:01 -0400

    Header Banner and Site Background

--- client/src/components/header/header.jsx
+++ client/src/components/header/header.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import './header.scss';
-import banner from '../../assets/images/val-banner-dark.jpg';
+import banner from '../../assets/images/val-banner-dark.png';
 import Navigation from '../Navigation/Navigation';
 
 function Header() {
     return (
         <div className="header">
             <img src={banner} className="header__banner" alt="Valorant Banner"/>
-            <h1 className="header__name header__name1">Chawpsticks #NA1</h1>
-            <h1 className="header__name header__name2">Chawpsticks #NA1</h1>
+            <div className="header__name-container header__name-container1">
+                <h1 className="header__name header__name1">Chawpsticks #NA1</h1>
+            </div>
+            <div className="header__name-container header__name-container2">
+                <h1 className="header__name header__name2">Chawpsticks #NA1</h1>
+            </div>
             <Navigation />
             <p className="text">Blend your style and experience on a global, competitive stage. You have 13 rounds to attack and defend your side using sharp gunplay and tactical abilities. And, with one life per-round, you'll need to think faster than your opponent if you want to survive. Take on foes across Competitive and Unranked modes as well as Deathmatch and Spike Rush.</p>
             
