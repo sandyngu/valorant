c9860de5f4498a528b92c95b9ae6a19aff9affd8
Sandy Nguyen <69399429+sandyngu@users.noreply.github.com>
2021-07-05 23:59:13 -0400

    API Keys

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -3,6 +3,10 @@ import Jett from '../../assets/images/jett.gif';
 import './home.scss';
 
 function Home() {
+
+    const trackerAPIKey = "75000a71-51ac-4a26-8f83-87119556c816";
+    const riotAPIKey = "RGAPI-d3c98792-673b-4a52-bd41-96ce5ec0dd01";
+
     
     return (
         <div className="home">
