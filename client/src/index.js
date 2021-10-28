c8944dc93a3d3c7721a0a73b4602a521b76acc0a
Sandy Nguyen <69399429+sandyngu@users.noreply.github.com>
2021-10-28 16:20:28 -0400

    Trying to Fix Index.html

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <BrowserRouter basename={window.location.pathname || ''}>
@@ -18,3 +19,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+serviceWorker.unregister();
