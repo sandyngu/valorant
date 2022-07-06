e5909eba924ece65a8c22211338f614d15678ec7
Sandy Nguyen <69399429+sandyngu@users.noreply.github.com>
2022-07-06 16:19:17 -0400

    Font Changed Back to DINNEXTW1G, Buttons Adjusted

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -36,7 +36,7 @@ function Home() {
                     <div className="home__heading">Hello, welcome to <br/> Chawpsticks' <br/><span className="home__heading-val">Valorant</span> website.</div>
                 </div>
                 <div className="home__text-container">
-                    <div className="home__text"><span className="home__text-first">Valorant is a 5v5 character-based tactical shooter.</span> 
+                    <div className="home__text"><span className="home__text-first">VALORANT IS A 5v5 CHARACTER-BASED TACTICAL SHOOTER.</span> 
                         <br/><br/>Here you will be blessed with some of my highlight reels and my friends' highlight reels from the game. 
                         <span className="home__text--strike">I am hardstuck gold.</span> <span className="home__text--strike">I am finally plat!
                         </span> <span className="home__text--strike">I'm gold again...</span> I'M PLAT! and have spent an unhealthy amount of time on the game. Tracker.gg says I have spent 1300+ hours on the game. I have been playing since beta.
@@ -45,13 +45,13 @@ function Home() {
                     <div className="home__text-line"></div>
                     <div className="home__text-button-container">
                         <div className="home__text-button-box home__text-button-box1"></div>
-                        <button className="home__text-button home__text-button1" onClick={() => clipsPage()}>View Chawp's Clips</button>
+                        <button className="home__text-button home__text-button1" onClick={() => clipsPage()}>VIEW CHAWP'S CLIPS</button>
                         <div className="home__text-button-box home__text-button-box2"></div>
-                        <button className="home__text-button home__text-button2" onClick={() => friendsClipsPage()}>View My Friends' Clips</button>
+                        <button className="home__text-button home__text-button2" onClick={() => friendsClipsPage()}>VIEW MY FRIENDS' CLIPS</button>
                         <div className="home__text-button-box home__text-button-box3"></div>
-                        <button className="home__text-button home__text-button3" onClick={() => agentAnthemsPage()}>Listen to Agent Anthems</button>
+                        <button className="home__text-button home__text-button3" onClick={() => agentAnthemsPage()}>LISTEN TO AGENT ANTHEMS</button>
                         <div className="home__text-button-box home__text-button-box4"></div>
-                        <button className="home__text-button home__text-button4" onClick={() => galleryPage()}>View The Gallery</button>
+                        <button className="home__text-button home__text-button4" onClick={() => galleryPage()}>VIEW THE GALLERY</button>
                     </div>
                 </div>
                 <img src={Jett} alt="Jett" className="home__jett"/>
--- client/src/components/navigation/navigation.jsx
+++ client/src/components/navigation/navigation.jsx
@@ -13,16 +13,16 @@ function Navigation(props) {
                         <img src={Logo} alt="Logo" className="nav-list-logo"/>
                     </Link>/ /
                     <Link to='/clips' className="nav__link" onClick={() => props.changeClipsBanner()}>
-                        <li className="nav-list__link nav-list__link-clips">Chawp's Clips</li>
+                        <li className="nav-list__link nav-list__link-clips">CHAWP'S CLIPS</li>
                     </Link>/ /
                     <Link to='/friendsclips' className="nav__link" onClick={() => props.changeFriendsBanner()}>
-                        <li className="nav-list__link nav-list__link-friendsclips">Friends' Clips</li>
+                        <li className="nav-list__link nav-list__link-friendsclips">FRIENDS' CLIPS</li>
                     </Link>/ /
                     <Link to='/agentanthems' className="nav__link" onClick={() => props.changeAgentsBanner()}>
-                        <li className="nav-list__link nav-list__link-agentanthems">Agent Anthems</li>
+                        <li className="nav-list__link nav-list__link-agentanthems">AGENT ANTHEMS</li>
                     </Link>/ /
                     <Link to='/gallery' className="nav__link" onClick={() => props.changeGalleryBanner()}>
-                        <li className="nav-list__link nav-list__link-gallery">Gallery</li>
+                        <li className="nav-list__link nav-list__link-gallery">GALLERY</li>
                     </Link>
                     <Link to='/upload' className="nav__link-upload" onClick={() => props.changeUploadBanner()}></Link>
                 </ul>
